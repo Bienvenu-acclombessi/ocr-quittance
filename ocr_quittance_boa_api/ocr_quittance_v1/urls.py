7a2245f81346aec993bee2ed7cb6ecf81d170047
Welcom@kode <bienvenuacclombessi8@gmail.com>
2024-06-30 15:24:04 +0100

    feat: change domain

--- ocr_quittance_boa_api/ocr_quittance_v1/urls.py
+++ ocr_quittance_boa_api/ocr_quittance_v1/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import ProcessPDFView
 
 urlpatterns = [
-    path('process_pdf/', ProcessPDFView.as_view(), name='process_pdf'),
+    path('v1/', ProcessPDFView.as_view(), name='process_pdf'),
 ]
