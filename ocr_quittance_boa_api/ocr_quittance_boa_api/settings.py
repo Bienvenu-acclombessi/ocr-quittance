d5aa3ff44e0a7df165b95de3fbb3e16f165a708a
Welcom@kode <bienvenuacclombessi8@gmail.com>
2024-06-30 15:19:06 +0100

    feat: add domain to setting without localhost

--- ocr_quittance_boa_api/ocr_quittance_boa_api/settings.py
+++ ocr_quittance_boa_api/ocr_quittance_boa_api/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = 'django-insecure-i8@w-%nt0=vxn+6)!i$d$pz^(0-ziyoe^obhaztj19e_wk!l&x
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ocrquittanceapi1-f17rbnap.b4a.run',
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
+    'ocrquittanceapi1-f17rbnap.b4a.run'
 ]
 
 
